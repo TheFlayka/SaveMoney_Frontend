e6ea984818664a44f19e3eb665483fbfd971efa3
Flayka <the.flayka@gmail.com>
2024-12-01 16:36:17 +0500

    Start

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template><p class="font-second-main">Fonts €</p></template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			main: ['Dela Gothic One'],
+			'second-main': ['Poppins'],
+			currency: ['Trocchi'],
+		},
+	},
+	plugins: [],
+}
